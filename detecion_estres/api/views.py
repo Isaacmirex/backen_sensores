fefd75d1e98ffd61fd1dbf941de7c556fddd6e89
IsaacRomero <CatCoatGaming@gmail.com>
2024-04-22 21:03:09 -0500

    Actualizacion

--- detecion_estres/api/views.py
+++ detecion_estres/api/views.py
@@ -1,22 +1,15 @@
 from rest_framework import viewsets
-from rest_framework.response import Response
-from detecion_estres.models import Encuesta,Sensores,Usuario
-from detecion_estres.api.serializer import EncuestaSerializer , SensoresSerializer,UsuarioSerializer
+from detecion_estres.models import Encuesta, Sensores, Usuario
+from detecion_estres.api.serializer import EncuestaSerializer, SensoresSerializer, UsuarioSerializer
 
-class EncuestaViewSet(viewsets.ViewSet):
-    def list(self, request):
-        encuestas = Encuesta.objects.all()
-        serializer = EncuestaSerializer(encuestas, many=True)
-        return Response(serializer.data)
+class EncuestaViewSet(viewsets.ModelViewSet):
+    queryset = Encuesta.objects.all()
+    serializer_class = EncuestaSerializer
 
-class SensoresViewSet(viewsets.ViewSet):
-    def list(self, request):
-        sensores = Sensores.objects.all()
-        serializer = SensoresSerializer(sensores, many=True)
-        return Response(serializer.data)
+class SensoresViewSet(viewsets.ModelViewSet):
+    queryset = Sensores.objects.all()
+    serializer_class = SensoresSerializer
 
-class UsuarioViewSet(viewsets.ViewSet):
-    def list(self, request):
-        usuarios = Usuario.objects.all()
-        serializer = UsuarioSerializer(usuarios, many=True)
-        return Response(serializer.data)
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\isaacR\Documents\Tesis\desarrollo-20204\backend_utn\venv
+set VIRTUAL_ENV=D:\TESIS\REALIZACION\backen_django\backen_sensores\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
